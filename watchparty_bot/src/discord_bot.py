7cfe678235711996c27b7e49402a07fdcce76ef2
joaooliveira247 <joaooliveira247@gmail.com>
2022-09-21 21:36:59 -0300

    chore: make changes in poll function

--- watchparty_bot/src/discord_bot.py
+++ watchparty_bot/src/discord_bot.py
@@ -1,6 +1,7 @@
 from watchparty_bot.src import wrapper
+from watchparty_bot.src.utils import Utils
 from disnake.ext import commands
-from disnake import Embed, ChannelType, Thread, Message, TextChannel
+from disnake import Embed, ChannelType, Thread, Message, TextChannel, Colour
 from asyncio import sleep
 from unicodedata import normalize
 from requests import HTTPError
@@ -15,25 +16,24 @@ async def on_ready():
 
 
 @bot.slash_command(name="poll", description="Make an poll.")
-async def thread(command, args: str) -> Thread:
+async def poll(command, movies: str) -> Thread:
     if (
         "movie" not in command.channel.name.lower()
-        | command.channel.type != "text"
+        and command.channel.type != "text"
     ):
         return await command.response.send_message(
             "Looks like you're on the wrong channel."
         )
 
-    args: list[str] = args.split(",")
-    args: list[str] = [x.replace(":", "") for x in args]
+    movies: list[str] = movies.split(",")
+    movies: list[str] = [movie.replace(":", "") for movie in movies]
 
-    # inter.response.send_message
     await command.response.send_message(
-        f"Starting a poll with: {''.join(args).replace(' ', ', ').title()}"
+        f"Starting a poll with: {', '.join(movies).title()}"
     )
 
     thread: Thread = await command.channel.create_thread(
-        name=f"{''.join(args).replace(' ', ', ').title()} Poll",
+        name=f"{', '.join(movies).title()} Poll",
         auto_archive_duration=60,
         type=ChannelType.public_thread,
     )
@@ -47,37 +47,43 @@ async def thread(command, args: str) -> Thread:
         5: "5\uFE0F\u20E3",
     }
 
-    for arg in enumerate(args, start=1):
+    thread_messages: list[int] = []
+
+    for movie in enumerate(movies, start=1):
         try:
-            movie: dict[str, str] | list[
+            film: dict[str, str] | list[
                 dict[str, str]
             ] = wrapper.movie_wrapper(
-                normalize("NFD", arg[1])
+                normalize("NFD", movie[1])
                 .encode("ascii", "ignore")
                 .decode("utf8")
             )
         except HTTPError:
             return await command.channel.send("Movie not found.")
         embed_movie: Embed = Embed()
+        embed_movie.color = Colour.blurple()
         embed_movie.title = "{} - {}: {}".format(
-            numbers[arg[0]], movie["type"].title(), movie["title"]
+            numbers[movie[0]], film["type"].title(), film["title"]
         )
-        embed_movie.set_thumbnail(url=movie["poster"])
-        embed_movie.description = movie["description"]
+        embed_movie.set_thumbnail(url=film["poster"])
+        embed_movie.description = film["description"]
         embed_movie.set_footer(
             text="Genres: {}\nDate: {}\nRating: {}/10 \U0001f31f".format(
-                "".join(movie["genres"]), movie["created_at"], movie["rating"]
+                " | ".join(film["genres"]), film["created_at"], film["rating"]
             )
         )
 
-        await thread.send(embed=embed_movie)
+        thread_message: Message = await thread.send(embed=embed_movie)
+        thread_messages.append(thread_message.id)
 
     message: Message = await thread.send("Voting ...")
-    for i in range(len(args)):
+    for i in range(len(movies)):
         await message.add_reaction(numbers[i + 1])
     await sleep(15)
     # TODO: Make an for to get each emoji (parameters of reactions is a string of emoji.)
-    reactions: list[dict[str, str | int]] = []
+    reactions: list[str] = []
+
+    reactions_info: list[dict[str, str | int]] = []
 
     for reaction in bot.get_message(message.id).reactions:
         match reaction.me:
@@ -93,35 +99,45 @@ async def thread(command, args: str) -> Thread:
                 users = []
                 async for user in reaction.users():
                     users.append(user.name)
+                    reactions.append(reaction.emoji)
 
                 reaction_stats["users"] = users
                 reaction_stats["count"] = reaction.count
 
-                reactions.append(reaction_stats)
+                reactions_info.append(reaction_stats)
 
-    await message.channel.send(reactions)
+    winner: tuple[str, int] = Utils.frequentily(reactions)[0]
+
+    embed_winner: Embed = bot.get_message(
+        thread_messages[int(winner[0].replace("\uFE0F\u20E3", "")) - 1]
+    ).embeds[0]
+    embed_winner.color = Colour.green()
+    embed_winner.description = "Win with {} votes.".format(str(winner[1]))
+
+    await message.channel.send(embed=embed_winner)
+    #TODO: close thread.
 
 @bot.slash_command(
-    name="clear", description="Clear 100 messages in Movie text chat"
+    name="clear", description="Clear 100 messages in Movie text chat."
 )
 async def clear(command) -> TextChannel:
     if (
         "movie" not in command.channel.name.lower()
-        | command.channel.type != "text"
+        and command.channel.type != "text"
     ):
         return await command.response.send_message(
             "Looks like you're on the wrong channel."
         )
 
     await command.response.send_message(
-        "This process may take a while and will only delete 100 messages"
+        "This process may take a while and will only delete 100 messages."
     )
     count: int = 0
 
     async for _ in command.channel.history(limit=None):
         count += 1
 
-    await command.channel.send("This channel has {} messages".format(count))
+    await command.channel.send("This channel has {} messages.".format(count))
     await command.channel.send(command.channel.type)
 
     sleep(15)
