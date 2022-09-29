d0562b95a4f86447d2597c6f10bbaad8e6998967
joaooliveira247 <joaooliveira247@gmail.com>
2022-09-29 17:09:13 -0300

    feat: make functions to convert seconds to minutes.

--- watchparty_bot/src/utils.py
+++ watchparty_bot/src/utils.py
@@ -1,10 +1,16 @@
 from collections import Counter
+from unicodedata import normalize
+from datetime import timedelta
+
 
 class Utils:
     @staticmethod
-    def frequently(freq: list[str],) -> tuple[str, int]:
+    def frequently(
+        freq: list[str],
+    ) -> tuple[str, int]:
         return Counter(freq).most_common(1)
 
     @staticmethod
-    def seconds_to_minutes(time: float) -> float:
-        return round(time / 60.0, 2)
+    def seconds_to_minutes(time: float) -> str:
+        return str(timedelta(seconds=time))
+
