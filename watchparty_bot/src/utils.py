551b66d5c0e3bbbd2cd8a3103690f485cf73a5d2
joaooliveira247 <joaooliveira247@gmail.com>
2022-09-29 17:09:47 -0300

    feat: make functions to convert text.

--- watchparty_bot/src/utils.py
+++ watchparty_bot/src/utils.py
@@ -14,3 +14,16 @@ class Utils:
     def seconds_to_minutes(time: float) -> str:
         return str(timedelta(seconds=time))
 
+    @staticmethod
+    def clean_phrase(phrase: str | list[str]) -> str | list[str]:
+        if isinstance(phrase, str):
+            return (
+                normalize("NFD", phrase)
+                .encode("ascii", "ignore")
+                .decode("utf-8")
+            )
+        cleaned_phrases = []
+        for word in phrase:
+            cleaned_phrases.append(Utils.clean_phrase(word))
+
+        return cleaned_phrases
